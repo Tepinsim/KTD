3e69719e762e5704dd63c530dc30b02d06f6eee0
Charmzz <118677880+Tepinsim@users.noreply.github.com>
2025-03-17 14:21:56 +0700

    Add files via upload

---
+++ yolov8.py
@@ -0,0 +1,35 @@
+import streamlit as st
+from ultralytics import YOLO
+import cv2
+import numpy as np
+from PIL import Image
+
+def main():
+    st.title("Khmer Text Detection with YOLOv8")
+
+    uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        st.image(image, caption="Uploaded Image", use_column_width=True)
+
+        if st.button("Detect Khmer Text"):
+            with st.spinner("Detecting Khmer text..."):
+                # Convert PIL Image to OpenCV format
+                image_cv = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+
+                # Load the YOLOv8 model
+                model = YOLO("/Users/Cs-Store/Desktop/intern2/yolov8/173.pt")
+
+                # Perform inference
+                results = model(image_cv)
+
+                # Process and display the results
+                for r in results:
+                    im_array = r.plot()
+                    im = cv2.cvtColor(im_array, cv2.COLOR_BGR2RGB)
+                    st.image(im, caption="Detected Khmer Text", use_column_width=True)
+                st.success("Khmer text detection complete!")
+
+if __name__ == "__main__":
+    main()
